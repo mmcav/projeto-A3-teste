568ea2498a37473b2644d1d5d87ede40173bfb16
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2024-04-24 14:57:53 -0300

    build: fixed path to commit msg file

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ mkdir -p $PATH_TEMPLATE/hooks
 git config --global core.hooksPath $PATH_TEMPLATE/hooks
 git config --global init.templatedir $PATH_TEMPLATE
 
-cp ../commit-msg $PATH_TEMPLATE/hooks/
+cp ./commit-msg $PATH_TEMPLATE/hooks/
