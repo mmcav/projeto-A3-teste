8110b2c6e8ceaa3ace5c9e941e9053d00bbb5558
Marcelo Cavalcanti <19808040+mmcav@users.noreply.github.com>
2024-04-29 16:10:54 -0300

    fix: deleting a file (no need to pretend this is a fix)

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PATH_TEMPLATE=~/.git-template
-
-mkdir -p $PATH_TEMPLATE/hooks
-
-git config --global core.hooksPath $PATH_TEMPLATE/hooks
-git config --global init.templatedir $PATH_TEMPLATE
-
-cp ./commit-msg $PATH_TEMPLATE/hooks/
